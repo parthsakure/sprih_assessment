96772a7642c36114812e14ff359d494a0ee57f74
parth Sakure <parthsakure01@gmail.com>
2025-12-01 19:09:49 +0530

    first commit

---
+++ src/main/java/com/assessment/sprih/SprihApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.sprih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/sprih/SprihApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.sprih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
